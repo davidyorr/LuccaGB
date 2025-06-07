e22ce26867f9914d31a9dfe5399f41a764407f3b
David Orr <davidyorr@gmail.com>
2025-06-07 03:36:17 -0400

    Add prettier

--- src/echogb.ts
+++ src/echogb.ts
@@ -1,5 +1,7 @@
 const go = new Go();
 
-WebAssembly.instantiateStreaming(fetch("main.wasm"), go.importObject).then((wasmModule) => {
-    go.run(wasmModule.instance);
-})
+WebAssembly.instantiateStreaming(fetch("main.wasm"), go.importObject).then(
+	(wasmModule) => {
+		go.run(wasmModule.instance);
+	},
+);
