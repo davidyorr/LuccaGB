ffabeb8e01a345aba463b784d6df337bf20f9f61
David Orr <davidyorr@gmail.com>
2025-06-22 02:06:11 -0400

    Use a shared counter

--- timer/timer.go
+++ timer/timer.go
@@ -1,8 +1,6 @@
 package timer
 
 type Timer struct {
-	// 0xFF04 divider register
-	div uint8
 	// 0xFF05 timer counter
 	tima uint8
 	// 0xFF06 timer modulo
@@ -10,8 +8,8 @@ type Timer struct {
 	// 0xFF07 timer control
 	tac uint8
 
-	divCounter  uint16
-	timaCounter uint16
+	counter               uint16
+	previousTimerBitState bool
 }
 
 func New() *Timer {
@@ -21,59 +19,46 @@ func New() *Timer {
 }
 
 func (timer *Timer) Reset() {
-	timer.div = 0x18
-	timer.divCounter = 0
 	timer.tima = 0x00
 	timer.tma = 0x00
 	timer.tac = 0xF8
+	timer.counter = 0xABCC
+	timer.previousTimerBitState = false
 }
-
-// 4194304 Hz / 16384 Hz
-const threshold = 256
 
 // return value of true represents a signal interrupt request
 func (timer *Timer) Step(cycles uint8) bool {
+	var interrupt bool = false
 	cycles16 := uint16(cycles)
-	timer.divCounter += cycles16
-	timer.timaCounter += cycles16
 
-	if timer.divCounter >= threshold {
-		timer.div++
-		timer.divCounter -= threshold
-	}
+	for cycles16 > 0 {
+		if timer.isTimerEnabled() {
+			currentTimerBitState := timer.getTimerBitState()
 
-	// bit 2 -> timer enabled
-	if (timer.tac & 0b0000_0100) != 0 {
-		var frequency uint16 = 1024
-		switch timer.tac & 0b11 {
-		case 0b00:
-			frequency = 1024
-		case 0b01:
-			frequency = 16
-		case 0b10:
-			frequency = 64
-		case 0b11:
-			frequency = 256
-		}
-
-		for timer.timaCounter >= frequency {
-			timer.timaCounter -= frequency
-			timer.tima++
-			// check for overflow
-			if timer.tima == 0 {
-				timer.tima = timer.tma
-				return true
+			if timer.previousTimerBitState && !currentTimerBitState {
+				timer.tima++
+				// check for overflow
+				if timer.tima == 0 {
+					timer.tima = timer.tma
+					interrupt = true
+				}
 			}
+
+			timer.previousTimerBitState = currentTimerBitState
 		}
+
+		timer.counter++
+		cycles16--
 	}
 
-	return false
+	return interrupt
+
 }
 
 func (timer *Timer) Read(address uint16) uint8 {
 	switch address & 0x000F {
 	case 0x04:
-		return timer.div
+		return uint8(timer.counter >> 8)
 	case 0x05:
 		return timer.tima
 	case 0x06:
@@ -89,8 +74,7 @@ func (timer *Timer) Read(address uint16) uint8 {
 func (timer *Timer) Write(address uint16, value uint8) {
 	switch address & 0x000F {
 	case 0x04:
-		timer.div = 0
-		timer.divCounter = 0
+		timer.counter = 0
 	case 0x05:
 		timer.tima = value
 	case 0x06:
@@ -99,4 +83,29 @@ func (timer *Timer) Write(address uint16, value uint8) {
 		// upper 5 bits are unused
 		timer.tac = (value & 0b0000_0111)
 	}
+}
+
+func (timer *Timer) getTimerBitState() bool {
+	// the bit index to check on the counter
+	var bitIndex uint8 = 9
+
+	switch timer.tac & 0b11 {
+	case 0b00:
+		bitIndex = 9
+	case 0b01:
+		bitIndex = 3
+	case 0b10:
+		bitIndex = 5
+	case 0b11:
+		bitIndex = 7
+	}
+
+	mask := uint16(1 << bitIndex)
+
+	return (timer.counter & mask) != 0
+}
+
+func (timer *Timer) isTimerEnabled() bool {
+	// bit 2 -> timer enabled
+	return (timer.tac & 0b0000_0100) != 0
 }
