756f4418018fdd89b560f1d955c7d47ec6a684d3
David Orr <davidyorr@gmail.com>
2025-06-26 19:27:43 -0400

    Add more ppu fields

--- ppu/ppu.go
+++ ppu/ppu.go
@@ -1,13 +1,29 @@
 package ppu
 
 type PPU struct {
+	// 0x8000 - 0x97FF
+	vramTileData [6144]uint8
+
 	// 0x8000 - 9FFF
 	videoRam [8192]uint8
 	// 0xFE00 - 0xFE9F
 	oam [160]uint8
+	// 0xFF40 - LCDC: LCD control
+	lcdc uint8
 	// 0xFF44 - LY: LCD Y coordinate [read-only]
 	ly uint8
-
+	// 0xFF45 - LYC: LY compare
+	lyc uint8
+	// 0xFF41 - STAT: LCD status
+	stat uint8
+	// 0xFF42 - SCY: Background viewport Y position
+	scy uint8
+	// 0xFF43 - SCX: Background viewport X position
+	scx uint8
+	// 0xFF4A - WY: Window Y position
+	wy uint8
+	// 0xFF4B - WX: Window X position plus 7
+	wx      uint8
 	counter uint16
 }
 
@@ -23,7 +39,14 @@ func New() *PPU {
 }
 
 func (ppu *PPU) Reset() {
+	ppu.lcdc = 0x91
 	ppu.ly = 0x91
+	ppu.lyc = 0x00
+	ppu.stat = 0x81
+	ppu.scy = 0x00
+	ppu.scx = 0x00
+	ppu.wy = 0x00
+	ppu.wx = 0x00
 	ppu.counter = 0
 }
 
