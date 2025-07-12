268ede6c9040c15226d23227179332ae46f23c6f
David Orr <davidyorr@gmail.com>
2025-07-11 22:12:05 -0400

    Add a GitHub Actions file to generate a report of the tests

--- gameboy/gameboy_test.go
+++ gameboy/gameboy_test.go
@@ -18,7 +18,14 @@ const (
 	TestTypeMooneye TestType = "mooneye"
 )
 
+func skipCi(t *testing.T, romName string) {
+	if os.Getenv("CI") != "" {
+		t.Skipf("SKIPPING TESTCASE: %s.gb", romName)
+	}
+}
+
 func TestBlargg__cpu_instrs(t *testing.T) {
+	skipCi(t, "blargg/cpu_instrs")
 	loadRomAndRunSteps(t, "blargg/cpu_instrs", 25_000_000, TestTypeBlargg)
 }
 
@@ -38,27 +45,43 @@ func TestMooneye__add_sp_e_timing(t *testing.T) {
 	loadRomAndRunSteps(t, "mooneye/add_sp_e_timing", 140_335, TestTypeMooneye)
 }
 
+func TestMooneye__di_timing_GS(t *testing.T) {
+	loadRomAndRunSteps(t, "mooneye/di_timing-GS", 269_971, TestTypeMooneye)
+}
+
+func TestMooneye__div_timing(t *testing.T) {
+	loadRomAndRunSteps(t, "mooneye/div_timing", 185_726, TestTypeMooneye)
+}
+
+func TestMooneye__ei_sequence(t *testing.T) {
+	loadRomAndRunSteps(t, "mooneye/ei_sequence", 185_642, TestTypeMooneye)
+}
+
+func TestMooneye__ei_timing(t *testing.T) {
+	loadRomAndRunSteps(t, "mooneye/ei_timing", 185_635, TestTypeMooneye)
+}
+
 func TestMooneye__instr__daa(t *testing.T) {
-	loadRomAndRunSteps(t, "mooneye/instr/daa", 500_000, TestTypeMooneye)
+	loadRomAndRunSteps(t, "mooneye/instr/daa", 392_793, TestTypeMooneye)
 }
 
 func TestMooneye__ppu__hblank_ly_scx_timing_GS(t *testing.T) {
-	loadRomAndRunSteps(t, "mooneye/ppu/hblank_ly_scx_timing-GS", 540_335, TestTypeMooneye)
+	loadRomAndRunSteps(t, "mooneye/ppu/hblank_ly_scx_timing-GS", 300_000, TestTypeMooneye)
 }
 
 func TestMooneye__ppu__intr_1_2_timing_GS(t *testing.T) {
-	loadRomAndRunSteps(t, "mooneye/ppu/intr_1_2_timing-GS", 540_335, TestTypeMooneye)
+	loadRomAndRunSteps(t, "mooneye/ppu/intr_1_2_timing-GS", 238_312, TestTypeMooneye)
 }
 
 func TestMooneye__ppu__intr_2_0_timing(t *testing.T) {
-	loadRomAndRunSteps(t, "mooneye/ppu/intr_2_0_timing", 540_335, TestTypeMooneye)
+	loadRomAndRunSteps(t, "mooneye/ppu/intr_2_0_timing", 300_000, TestTypeMooneye)
 }
 func TestMooneye__ppu__intr_2_mode0_timing_sprites(t *testing.T) {
-	loadRomAndRunSteps(t, "mooneye/ppu/intr_2_mode0_timing_sprites", 540_335, TestTypeMooneye)
+	loadRomAndRunSteps(t, "mooneye/ppu/intr_2_mode0_timing_sprites", 400_000, TestTypeMooneye)
 }
 
 func TestMooneye__ppu__intr_2_mode0_timing(t *testing.T) {
-	loadRomAndRunSteps(t, "mooneye/ppu/intr_2_mode0_timing", 540_335, TestTypeMooneye)
+	loadRomAndRunSteps(t, "mooneye/ppu/intr_2_mode0_timing", 400_000, TestTypeMooneye)
 }
 
 func TestMooneye__ppu__intr_2_mode3_timing(t *testing.T) {
@@ -66,10 +89,11 @@ func TestMooneye__ppu__intr_2_mode3_timing(t *testing.T) {
 }
 
 func TestMooneye__ppu__vblank_stat_intr_GS(t *testing.T) {
-	loadRomAndRunSteps(t, "mooneye/ppu/vblank_stat_intr-GS", 420_758, TestTypeMooneye)
+	loadRomAndRunSteps(t, "mooneye/ppu/vblank_stat_intr-GS", 400_000, TestTypeMooneye)
 }
 
 func loadRomAndRunSteps(t *testing.T, romName string, stepCount int, testType TestType) {
+	t.Logf("TESTCASE: %s.gb", romName)
 	logBuffer, testLogger := initTestLogger()
 	logger.Init(testLogger.Handler())
 	defer logger.Init(slog.Default().Handler())
@@ -92,6 +116,9 @@ func loadRomAndRunSteps(t *testing.T, romName string, stepCount int, testType Te
 		passed, failed := checkResult(output, testType)
 		if failed {
 			t.Logf("❌ TEST FAILED after %d steps", i+1)
+			for _, line := range logBuffer.LastN(40) {
+				fmt.Fprint(os.Stdout, line)
+			}
 			t.Fatal()
 		}
 		if passed {
@@ -107,8 +134,10 @@ func loadRomAndRunSteps(t *testing.T, romName string, stepCount int, testType Te
 		for _, line := range logBuffer.LastN(40) {
 			fmt.Fprint(os.Stdout, line)
 		}
-		if err := os.WriteFile("../testoutput.log", []byte(strings.Join(logBuffer.messages, "")), 0644); err != nil {
-			fmt.Printf("Failed to write test output: %v\n", err)
+		if os.Getenv("CI") != "" {
+			if err := os.WriteFile("../testoutput.log", []byte(strings.Join(logBuffer.messages, "")), 0644); err != nil {
+				fmt.Printf("Failed to write test output: %v\n", err)
+			}
 		}
 		t.Logf("❌ TIMED OUT after %d steps", stepCount)
 		t.Fatal("Test timed out (no pass/fail signal detected)")
