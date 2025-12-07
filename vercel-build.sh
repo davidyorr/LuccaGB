4826f3bfe04cae165a68d9e4ce81a721cbe0b6de
David Orr <davidyorr@gmail.com>
2025-12-06 20:25:35 -0500

    Add a build script for vercel

---
+++ vercel-build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# download go
+curl -L -o go.tar.gz https://go.dev/dl/go1.24.3.linux-amd64.tar.gz
+
+# extract it
+tar -C . -xzf go.tar.gz
+
+# add local Go binary to PATH
+export PATH="$(pwd)/go/bin:$PATH"
+
+# verify installation
+go version
+
+# run the package.json build script
+pnpm run build
